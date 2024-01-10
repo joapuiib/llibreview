50da732c2d867fa5f1fe0ba5969c46f237a4bc50
Joan Puigcerver <joapuiib@gmail.com>
2024-01-10 11:48:09 +0100

    Commit inicial

---
+++ src/main/java/com/fpmislata/daw1/projectedaw1/ProjecteDaw1Application.java
@@ -0,0 +1,13 @@
+package com.fpmislata.daw1.projectedaw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecteDaw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecteDaw1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/fpmislata/daw1/projectedaw1/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.fpmislata.daw1.projectedaw1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
