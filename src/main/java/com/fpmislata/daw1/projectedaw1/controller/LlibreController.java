3eee472e6f7dd510103b3302b62d506e68b93321
Joan Puigcerver <joapuiib@gmail.com>
2024-04-12 21:35:44 +0200

    view: vista llibre

---
+++ src/main/java/com/fpmislata/daw1/projectedaw1/controller/LlibreController.java
@@ -0,0 +1,46 @@
+package com.fpmislata.daw1.projectedaw1.controller;
+
+import com.fpmislata.daw1.projectedaw1.common.container.LlibreIoc;
+import com.fpmislata.daw1.projectedaw1.controller.components.CardItem;
+import com.fpmislata.daw1.projectedaw1.domain.entity.Llibre;
+import com.fpmislata.daw1.projectedaw1.domain.service.LlibreService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class LlibreController {
+
+    private final LlibreService llibreService;
+
+    public LlibreController() {
+        this.llibreService = LlibreIoc.createLlibreService();
+    }
+
+    @SuppressWarnings("SameReturnValue")
+    @GetMapping("/llibre")
+    public String index(Model model) {
+        List<CardItem> llibres = llibreService.findAll().stream().map(
+                llibre -> {
+                     CardItem card = new CardItem();
+                     card.setTitol(llibre.getTitol());
+                     card.setSubtitol("ISBN: " + llibre.getIsbn());
+                     card.setUrl("/llibre/" + llibre.getIsbn());
+                     card.setImatgeUrl("/img/llibre/" + (llibre.getRutaImatge() != null ? llibre.getRutaImatge() : "fallback.png"));
+                     return card;
+                }).toList();
+        model.addAttribute("llibres", llibres);
+
+        return "llibre/llibres";
+    }
+
+    @GetMapping("/llibre/{isbn}")
+    public String llibre(Model model, @PathVariable(value = "isbn") String isbn) {
+        Llibre llibre = llibreService.findByIsbn(isbn);
+        model.addAttribute("llibre", llibre);
+        return "llibre/llibre";
+    }
+}
