f55855b1fddd986a17258c54881ef1e0d00ac969
Joan Puigcerver <joapuiib@gmail.com>
2024-05-15 23:35:33 +0200

    register page

--- src/main/java/com/fpmislata/daw1/projectedaw1/controller/LoginController.java
+++ src/main/java/com/fpmislata/daw1/projectedaw1/controller/LoginController.java
@@ -1,14 +1,12 @@
 package com.fpmislata.daw1.projectedaw1.controller;
 
 import com.fpmislata.daw1.projectedaw1.common.container.UserIoc;
-import com.fpmislata.daw1.projectedaw1.controller.components.CardItem;
 import com.fpmislata.daw1.projectedaw1.domain.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Collections;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -21,7 +19,26 @@ public class LoginController {
 
     @SuppressWarnings("SameReturnValue")
     @GetMapping("/login")
-    public String index(Model model) {
+    public String login(Model model) {
         return "login/login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        return "login/register";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam("username") String username,
+                           @RequestParam("email") String email,
+                           @RequestParam("password") String password,
+                           @RequestParam("passwordConfirmation") String passwordConfirmation) {
+
+        System.out.println("username: " + username);
+        System.out.println("email: " + email);
+        System.out.println("password: " + password);
+        System.out.println("password confirm: " + passwordConfirmation);
+        // userService.register(username, password, email);
+        return "redirect:/login/register";
     }
 }
